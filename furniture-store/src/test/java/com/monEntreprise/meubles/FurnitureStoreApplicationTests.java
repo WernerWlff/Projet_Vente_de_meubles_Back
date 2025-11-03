7968bc2f6791f9eb382e6c9529903831b250e9cd
Werner Wolff <werner.wolff1109@gmail.com>
2025-11-03 12:00:40 +0100

    add : Spring Initializr setup - Spring Security - Spring Web - Spring Data JPA - Flyway Migration
    add: Link to the database in the application.properties file

---
+++ furniture-store/src/test/java/com/monEntreprise/meubles/FurnitureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monEntreprise.meubles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
